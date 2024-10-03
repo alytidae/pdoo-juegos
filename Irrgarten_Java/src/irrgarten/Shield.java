1ebbcd9f7f6e8b3fdc1fc04029872fffc0b8d551
Igor Botukhov <igor.botukhov@gmail.com>
2024-10-03 20:45:51 +0200

    Added class Shield

---
+++ Irrgarten_Java/src/irrgarten/Shield.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package irrgarten;
+
+
+public class Shield {
+    private float protection;
+    private int uses;
+    
+    public Shield(float protection, int uses){
+        this.protection = power;
+        this.uses = uses;
+    }
+
+    public float protect() {
+        if (uses > 0) {
+            uses--; 
+            return protection; 
+        } else {
+            return 0;
+        }
+    }
+    
+    public String toString() {
+        return "S[" + protection + ", " + uses + "]";
+    }
+}
